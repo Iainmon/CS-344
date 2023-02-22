e883151ac44bf0ef5942480b6dea1657e82e0b21
Iain Moncrief <Iainmoncrief@gmail.com>
2023-02-21 20:22:51 -1000

    added comments

--- smallsh/main.c
+++ smallsh/main.c
@@ -442,25 +442,22 @@ int main(int argc, char* argv[]) {
 
 // Helper function I found for replacing a substring with another string 
 char *str_replace(char *orig, char *rep, char *with) {
-    char *result; // the return string
-    char *ins;    // the next insert point
-    char *tmp;    // varies
-    int len_rep;  // length of rep (the string to remove)
-    int len_with; // length of with (the string to replace rep with)
-    int len_front; // distance between rep and end of last rep
-    int count;    // number of replacements
-
-    // sanity checks and initialization
+    char *result; 
+    char *ins;
+    char *tmp;
+    int len_rep;
+    int len_with;
+    int len_front;
+    int count;
     if (!orig || !rep)
         return NULL;
     len_rep = strlen(rep);
     if (len_rep == 0)
-        return NULL; // empty rep causes infinite loop during count
+        return NULL;
     if (!with)
         with = "";
     len_with = strlen(with);
 
-    // count the number of replacements needed
     ins = orig;
     for (count = 0; tmp = strstr(ins, rep); ++count) {
         ins = tmp + len_rep;
@@ -476,7 +473,7 @@ char *str_replace(char *orig, char *rep, char *with) {
         len_front = ins - orig;
         tmp = strncpy(tmp, orig, len_front) + len_front;
         tmp = strcpy(tmp, with) + len_with;
-        orig += len_front + len_rep; // move to next "end of rep"
+        orig += len_front + len_rep;
     }
     strcpy(tmp, orig);
     return result;
